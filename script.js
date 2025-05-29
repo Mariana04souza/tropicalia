620810e76ed2fdf245e7c57587aae45210fec365
Mariana04souza <164515583+Mariana04souza@users.noreply.github.com>
2025-05-29 10:49:29 -0300

    aula 3

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded'),function(){
+    const botaoDeAcssibilidade = document.getElementById('botao-acenssibilidade')
+    const botaoDeAcssibilidade = document.getElementById('opcao-acessibilidade')
+
+    botaoDeAcssibilidade.addEventListener('click', function({
+        botaoDeAcssibilidade.classList.toggle('rotacao-botao')
+        opcoesDeAacessibilidade.classList.toggle('apresemta-lista')
+            
+        
+            
+        
+    
